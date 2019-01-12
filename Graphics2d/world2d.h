b9bd1bf3c1b0076e7d0f3ce98a785f4c4fae431d
何宏臻 <1242962912@qq.com>
2019-01-12 19:46:13 +0800

    修复

--- Graphics2d/world2d.h
+++ Graphics2d/world2d.h
@@ -1,13 +1,11 @@
 #ifndef GRAPHICS2D_WORLD2D_H
 #define GRAPHICS2D_WORLD2D_H
 
-#include "stdafx.h"
-
 class Helper2d;
 
-namespace lib2d 
+namespace lib2d
 {
-	static const auto inf = std::numeric_limits<double>::infinity();
+    static const auto inf = std::numeric_limits<double>::infinity();
 
     //二维向量
     struct v2
@@ -15,34 +13,34 @@ namespace lib2d
         double x{ 0 }, y{ 0 };
 
         v2() = default;
-		v2(double _x, double _y);
+        v2(double _x, double _y);
         v2(const v2 &v) = default;
         v2 &operator= (const v2 &v) = default;
 
-		v2 operator* (double d) const;
-		v2 operator/ (double d) const;
-		v2 operator+ (double d) const;
-		v2 operator- (double d) const;
-		v2 operator+ (const v2 &v) const;
-		v2 operator- (const v2 &v) const;
-		v2 &operator+= (const v2 &v);
-		v2 &operator-= (const v2 &v);
-		v2 operator- () const;
-		friend v2 operator* (double d, const v2 &v);
+        v2 operator* (double d) const;
+        v2 operator/ (double d) const;
+        v2 operator+ (double d) const;
+        v2 operator- (double d) const;
+        v2 operator+ (const v2 &v) const;
+        v2 operator- (const v2 &v) const;
+        v2 &operator+= (const v2 &v);
+        v2 &operator-= (const v2 &v);
+        v2 operator- () const;
+        friend v2 operator* (double d, const v2 &v);
 
-		double cross(const v2 &v) const;		// 叉乘
-		double dot(const v2 &v) const;			// 点乘
-        
-		double magnitude() const;				// 向量的长度
-		double magnitudeSquare() const;
+        double cross(const v2 &v) const;		// 叉乘
+        double dot(const v2 &v) const;			// 点乘
 
-		v2 normalize() const;					// 方向向量
-		v2 normal() const;						// 法线向量
-		v2 N() const;
+        double magnitude() const;				// 向量的长度
+        double magnitudeSquare() const;
 
-		bool zero(double d) const;				// 判断向量长度是否为0
+        v2 normalize() const;					// 方向向量
+        v2 normal() const;						// 法线向量
+        v2 N() const;
 
-		v2 rotate(double theta) const;			// 旋转
+        bool zero(double d) const;				// 判断向量长度是否为0
+
+        v2 rotate(double theta) const;			// 旋转
     };
 
     //二维矩阵
@@ -51,20 +49,20 @@ namespace lib2d
         double x1{ 1 }, y1{ 0 }, x2{ 0 }, y2{ 1 };
 
         m2() = default;
-		m2(double _x1, double _y1, double _x2, double _y2);
+        m2(double _x1, double _y1, double _x2, double _y2);
         m2(const m2 &m) = default;
         m2 &operator= (const m2 &m) = default;
-		m2(double d);
-        
-		m2 operator+ (const m2 &m) const;		//矩阵相加
-		v2 operator* (const v2 &v) const;		//矩阵相乘
-		m2 operator* (double d) const;			//数乘
-		friend m2 operator* (double d, const m2 &m);
+        m2(double d);
 
-		const m2 rotate(double theta);			//构造旋转矩阵
-		v2 rotate(const v2 &v)const;
-		double det() const;						//行列式的值
-		m2 inv() const;							//行列式求逆
+        m2 operator+ (const m2 &m) const;		//矩阵相加
+        v2 operator* (const v2 &v) const;		//矩阵相乘
+        m2 operator* (double d) const;			//数乘
+        friend m2 operator* (double d, const m2 &m);
+
+        const m2 rotate(double theta);			//构造旋转矩阵
+        v2 rotate(const v2 &v)const;
+        double det() const;						//行列式的值
+        m2 inv() const;							//行列式求逆
     };
 
 	class body2d
