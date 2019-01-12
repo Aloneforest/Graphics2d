e29714ac576c8c6b5e8c52b166d46e0e4b035204
何宏臻 <1242962912@qq.com>
2019-01-12 18:39:25 +0800

    修复

--- Graphics2d/Helper2d.cpp
+++ Graphics2d/Helper2d.cpp
@@ -6,8 +6,8 @@ QSizeF Helper2d::mid;
 
 Helper2d::Helper2d()
 {
-    //world.setHelper(this);
-    world.init();
+	world.setHelper(this);
+	world.init();
 }
 
 Helper2d::~Helper2d()
@@ -15,13 +15,13 @@ Helper2d::~Helper2d()
 
 }
 
-void Helper2d::paint(QPainterPtr painterPtr, QRect eventRect)
+void Helper2d::paint(QPainter * painter, QRect eventRect)
 {
-    this->rect = eventRect;
-    this->size = rect.size();
-    this->mid = size * 0.5;
-    this->painter = painter;
-    world.step(this);
+	this->rect = eventRect;
+	this->size = rect.size();
+	this->mid = size * 0.5;
+	this->painter = painter;
+	world.step(this);
 }
 
 void Helper2d::clear()
@@ -31,26 +31,27 @@ void Helper2d::clear()
 
 void Helper2d::paintLine(int x1, int y1, int x2, int y2)
 {
-    painter->setPen(drag);
-    painter->drawLine(QLineF(QPointF(x1, y1), QPointF(x2, y2)));
+	painter->setPen(drag);
+	painter->drawLine(QLineF(QPointF(x1, y1), QPointF(x2, y2)));
 }
 
 void Helper2d::paintPolygon(const std::vector<lib2d::v2> &v)
 {
-    static std::vector<QPointF> vp;
-    vp.clear();
-    vp.resize(v.size());
-    //std::transform(v.begin(), v.end(), vp.begin(), world2screen);	//第四个参数需要声明为静态函数
-    painter->setPen(drag);
-    painter->drawPolygon(vp.data(), (int)vp.size());
+	static std::vector<QPointF> vp;
+	vp.clear();
+	vp.resize(v.size());
+	std::transform(v.begin(), v.end(), vp.begin(), world2screen);	//第四个参数需要声明为静态函数
+	painter->setPen(drag);
+	painter->drawPolygon(vp.data(), (int)vp.size());
 }
 
 void Helper2d::paintText(int x, int y, const QString & str)
 {
-    painter->setPen(drag);
-    painter->setFont(text);
-    painter->drawText(x, y, str);
+	painter->setPen(drag);
+	painter->setFont(text);
+	painter->drawText(x, y, str);
 }
+
 
 lib2d::world2d Helper2d::getWorld()
 {
@@ -69,13 +70,13 @@ QRect Helper2d::getRect()
 
 QPointF Helper2d::world2screen(const lib2d::v2 & v)
 {
-    return QPointF((v.x + 1.0) * mid.width(), -v.y * mid.width() + mid.height());
+	return QPointF((v.x + 1.0) * mid.width(), -v.y * mid.width() + mid.height());
 }
 
-lib2d::v2 Helper2d::screen2world(const QPointF & pt)
-{
-    return lib2d::v2(pt.x() / mid.width() - 1.0, (mid.height() - pt.y() ) / mid.width());
-}
+//lib2d::v2 Helper2d::screen2world(const QPointF & pt)
+//{
+//    return lib2d::v2(pt.x() / mid.width() - 1.0, (mid.height() - pt.y() ) / mid.width());
+//}
 
 void Helper2d::exec(QString & str)
 {
